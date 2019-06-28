c4adb1d4a500c7c5d349fab6478cf09146a93baa
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-06-28 22:36:51 +0300

    Update to eslint 6

--- test/config.test.js
+++ test/config.test.js
@@ -1,8 +1,7 @@
 const test = require('ava');
-const eslint = require('eslint');
 
 test('load config in eslint to validate all rule syntax is correct', (t) => {
-  const { CLIEngine } = eslint;
+  const { CLIEngine } = require('eslint');
   const cli = new CLIEngine({ useEslintrc: false, configFile: '.eslintrc.json' });
   const code = 'const foo = 1;\nconst bar = () => {};\nbar(foo);\n';
   t.is(cli.executeOnText(code).errorCount, 0);
