4cdfe9b5063dce5cf9aa9a32c25d86d091fd20ba
Napoleon-Christos Oikonomou <napoleonoikon@gmail.com>
2024-04-05 21:21:10 +0300

    add @stylistic/disable-legacy plugin

--- react-typescript.js
+++ react-typescript.js
@@ -7,6 +7,7 @@ module.exports = {
 		"airbnb-typescript",
 		"plugin:@typescript-eslint/recommended-type-checked",
 		"plugin:@typescript-eslint/stylistic-type-checked",
+		"plugin:@stylistic/disable-legacy",
 	],
 	plugins,
 	parserOptions,
