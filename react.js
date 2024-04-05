d24552fc223e56f8c59f5a59e917fc657a4651b7
Napoleon-Christos Oikonomou <napoleonoikon@gmail.com>
2024-04-05 23:05:49 +0300

    use stylistic in more cases

--- react.js
+++ react.js
@@ -1,12 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-	extends: [
-		"airbnb",
-		"airbnb/hooks",
-		"plugin:react/recommended",
-		"plugin:jsx-a11y/recommended",
-		"./index",
-	],
+	extends: ["airbnb", "airbnb/hooks", "plugin:react/recommended", "plugin:jsx-a11y/recommended", "./index"],
 	env: {
 		browser: true,
 	},
@@ -31,16 +25,37 @@ module.exports = {
 		"react/jsx-curly-brace-presence": ["error", { props: "never", children: "always" }],
 		"react/jsx-indent": ["error", "tab", { checkAttributes: true, indentLogicalExpressions: true }],
 		"react/jsx-indent-props": ["error", "tab"],
-		"react/jsx-key": ["error", { checkFragmentShorthand: true, checkKeyMustBeforeSpread: true, warnOnDuplicates: true }],
+		"react/jsx-key": [
+			"error",
+			{
+				checkFragmentShorthand: true,
+				checkKeyMustBeforeSpread: true,
+				warnOnDuplicates: true,
+			},
+		],
 		"react/jsx-no-duplicate-props": ["error", { ignoreCase: false }],
 		"react/jsx-no-useless-fragment": "error",
 		"react/jsx-props-no-spreading": "off",
-		"react/jsx-sort-props": ["error", { callbacksLast: true, shorthandFirst: true, noSortAlphabetically: true, reservedFirst: true }],
+		"react/jsx-sort-props": [
+			"error",
+			{
+				callbacksLast: true,
+				shorthandFirst: true,
+				noSortAlphabetically: true,
+				reservedFirst: true,
+			},
+		],
 		"react/jsx-uses-react": "off",
 		"react/no-array-index-key": "off",
 		"react/no-unstable-nested-components": ["error", { allowAsProps: true }],
 		"react/react-in-jsx-scope": "off",
-		"react/require-default-props": ["error", { forbidDefaultForRequired: true, ignoreFunctionalComponents: true }],
-		"unicorn/filename-case": ["error", { cases: { kebabCase: true, pascalCase: true } }],
+		"react/require-default-props": [
+			"error",
+			{
+				forbidDefaultForRequired: true,
+				ignoreFunctionalComponents: true,
+			},
+		],
+		"unicorn/filename-case": ["error", { cases: [Object] }],
 	},
 };
