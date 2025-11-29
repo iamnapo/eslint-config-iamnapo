fa5575606eae827a8e19952cd3e46769342e4cb1
Napoleon-Christos Oikonomou <napoleonoikon@gmail.com>
2025-11-29 14:07:09 +0200

    update rules

--- configs/typescript.js
+++ configs/typescript.js
@@ -17,7 +17,10 @@ const config = [
 	{
 		rules: {
 			"import-x/extensions": ["error", { ignorePackages: true, checkTypeImports: true, pattern: { ts: "never" } }],
+			"import-x/no-named-as-default-member": "off",
 			"import-x/no-unresolved": "off",
+			"no-shadow": "off",
+			"no-use-before-define": "off",
 			"no-unused-vars": "off",
 			"no-void": ["error", { allowAsStatement: true }],
 			"require-await": "off",
@@ -25,6 +28,8 @@ const config = [
 			"@typescript-eslint/consistent-type-definitions": ["error", "type"],
 			"@typescript-eslint/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
 			"@typescript-eslint/no-import-type-side-effects": "error",
+			"@typescript-eslint/no-shadow": "error",
+			"@typescript-eslint/no-use-before-define": ["error", { functions: false }],
 			"@typescript-eslint/no-unused-vars": [
 				"error",
 				{ args: "after-used", ignoreRestSiblings: true, vars: "all", varsIgnorePattern: "^_" },
